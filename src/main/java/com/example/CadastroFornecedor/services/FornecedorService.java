e2ba0a7b6f6cf777466b531ff74970f4263bc03a
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-11 17:18:45 -0300

    exceptions

--- src/main/java/com/example/CadastroFornecedor/services/FornecedorService.java
+++ src/main/java/com/example/CadastroFornecedor/services/FornecedorService.java
@@ -2,7 +2,12 @@ package com.example.CadastroFornecedor.services;
 
 import com.example.CadastroFornecedor.entities.Fornecedor;
 import com.example.CadastroFornecedor.repositories.FornecedorRepository;
+import com.example.CadastroFornecedor.services.exceptions.DatabaseException;
+import com.example.CadastroFornecedor.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,7 +24,7 @@ public class FornecedorService {
 
     public Fornecedor findById(Long id) {
         Optional<Fornecedor> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public Fornecedor insert(Fornecedor obj) {
@@ -27,13 +32,26 @@ public class FornecedorService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public Fornecedor update(Long id, Fornecedor obj) {
-        Fornecedor entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            Fornecedor entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(Fornecedor entity, Fornecedor obj) {
