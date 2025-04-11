e1792370e6ad3a52087dd9cfe231a62929b81c0b
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-11 15:55:34 -0300

    h2 database, testprofile, injeção de dependencias

---
+++ src/main/java/com/example/CadastroFornecedor/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.example.CadastroFornecedor.config;
+
+import com.example.CadastroFornecedor.entities.Fornecedor;
+import com.example.CadastroFornecedor.repositories.FornecedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private FornecedorRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Fornecedor fornecedor = new Fornecedor(null, "Josepi", "SP", "24546456456", 1);
+        repository.save(fornecedor);
+    }
+}
